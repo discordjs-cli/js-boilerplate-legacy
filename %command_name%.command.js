6d4eae1a77da4463651f0c2c8a3668865482e2a7
baeonette <baeonette@gmail.com>
2022-08-24 01:12:22 -0700

    Initial commit

---
+++ %command_name%.command.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "%command_name%",
+    aliases: [],
+    description: "%command_name% command",
+    execute(message, args) {
+        message.reply('%command_name% works!');
+    }
+};
